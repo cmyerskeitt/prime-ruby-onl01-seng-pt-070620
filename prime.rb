93dad97bdd059c0eb21a1f524d78b81ed9bb24d2
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-15 18:00:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(number)
+  return false if !number.integer?
+  return false if the number < 2 
+  return true if number == 2 
+  (2..number-1).each do |int| return false if number % |int| == 0 )
+  true
+end 
+  
+  
+  
+  
+  
+  
+  
+#   (2..(num - 1)).each do |n|
+#     return false if num % |n| == 0
+#   end
+#   true
+# end
+  
